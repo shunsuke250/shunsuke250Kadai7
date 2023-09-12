1f49abb8743a5a8e768014311b775dcc1be9d43d
副山俊輔 <soeyamashunsuke@MacBook-Pro.local>
2023-09-12 23:31:13 +0900

    :sparkles: 減算機能を追加

---
+++ TabBarCalculator23-09-12/TabBarCalculator23-09-12/SubtractionViewController.swift
@@ -0,0 +1,36 @@
+//
+//  SubtractionViewController.swift
+//  TabBarCalculator23-09-12
+//
+//  Created by 副山俊輔 on 2023/09/12.
+//
+
+import UIKit
+
+class SubtractionViewController: UIViewController {
+
+    @IBOutlet private weak var firstTextField: UITextField!
+    @IBOutlet private weak var secondTextField: UITextField!
+    @IBOutlet private weak var subtraction: UIButton!
+    @IBOutlet private weak var resultLabel: UILabel!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        resultLabel.text = ""
+    }
+    
+    @IBAction private func didTapSubtraction(_ sender: Any) {
+        calculateSubtraction()
+        firstTextField.resignFirstResponder()
+        secondTextField.resignFirstResponder()
+    }
+    
+    private func calculateSubtraction() {
+        guard let firstValue = Int(firstTextField.text ?? ""),
+              let secondValue = Int(secondTextField.text ?? "")
+        else { return }
+        
+        let result = firstValue - secondValue
+        resultLabel.text = "\(result)"
+    }
+}
